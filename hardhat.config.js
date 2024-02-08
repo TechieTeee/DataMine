10e1598f1d0f03a4a6cd89c974550d055da41d1a
TechieTeee <techieteee@protonmail.com>
2024-02-08 23:10:10 +0000

    Updated deployment files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,28 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-web3");
+require("@nomicfoundation/hardhat-etherscan");
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    scrollSepolia: {
+      url: "https://rpc.sepolia.scroll.network",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
